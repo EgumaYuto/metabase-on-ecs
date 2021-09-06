edd869e6dd82f2b4b53d8c78a5b4e738bcbd3f1b
yuto.eguma <yuto.eguma@gmail.com>
2021-09-06 23:52:45 +0900

    bastion に postgresql クライアントをインストール

--- infra/devops/bastion/user_data.sh
+++ infra/devops/bastion/user_data.sh
@@ -3,4 +3,5 @@
 yum -y upgrade
 
 yum -y install https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+amazon-linux-extras install postgresql13
 systemctl restart amazon-ssm-agent
