d59eab8c54b17aabe2316e0f6ab553626fb41d96
yuto.eguma <yuto.eguma@gmail.com>
2021-09-08 01:46:58 +0900

    S3バケットを削除可能にする

--- infra/service/scheduler/s3.tf
+++ infra/service/scheduler/s3.tf
@@ -1,3 +1,5 @@
 resource "aws_s3_bucket" "source" {
-  bucket = "${module.naming.name}-source"
+  bucket        = "${module.naming.name}-source"
+  acl           = "private"
+  force_destroy = true
 }
